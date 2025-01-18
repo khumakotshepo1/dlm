dda66d1df70bef5ee809fb282643d7a633dc02c9
Tshepo Khumsko <khumakotshepo1@gmail.com>
2025-01-18 17:45:08 +0200

    Add data to executive page

--- src/app/about-us/government/executive/page.tsx
+++ src/app/about-us/government/executive/page.tsx
@@ -1,9 +1,21 @@
-import {MenuItems} from "@/components/menu-items";
-import ExecutiveData from "@/components/ExecutiveData";
-
+import { MenuItems } from "@/components/menu-items";
 
 const Executive = () => {
-    return <MenuItems data={ExecutiveData}/>;
+
+    const data = [
+        {
+            title: "Mayco",
+            link: "government/executive/mayco",
+            img: "/img/townhall.webp",
+        },
+        {
+            title: "Office of the Mayor",
+            link: "government/executive/office-of-the-mayor",
+            img: "/img/council/Cllr-Tseki-Tseki-Ward-14-ANC_11zon.webp",
+        },
+    ];
+
+    return <MenuItems data={data} />;
 };
 
 export default Executive;
